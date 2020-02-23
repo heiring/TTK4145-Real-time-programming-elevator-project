21aca09a39341c4b04b5b6b15499278287b511ab
Pathfinder <void>
2020-02-23 16:15:30 +0100

    FSM crawling

--- FSM/FSM.go
+++ FSM/FSM.go
@@ -1,5 +1,8 @@
 package FSM
 
+import "../elevio"
+import "fmt"
+
 type elev_state int
 
 const (
@@ -24,19 +27,62 @@ func StateTransistions(state elev_state) {
 	}
 }
 
-func FSM(state elev_state) {
-	switch state {
-	case INIT:
-	//elevator reaches first floor, transistion to IDLE
-	case IDLE:
-	//transitions to MOVE when an order is detected
-	//transisiton to EM_STOP
-	case MOVE:
-	//transisiton to wait when elevator reaches target floor
-	//transisiton to EM_stop
-	case WAIT:
-	//transisiton to MOVE if there are pending orders
-	//transition to IDLE if not
-	case EM_STOP:
+func FSM() {
+
+	var targetFloor int
+
+	numFloors := 4
+
+	elevio.Init("localhost:15657", numFloors)
+
+	drv_buttons := make(chan elevio.ButtonEvent)
+	drv_floors := make(chan int)
+	drv_obstr := make(chan bool)
+	drv_stop := make(chan bool)
+
+	go elevio.PollButtons(drv_buttons)
+	go elevio.PollFloorSensor(drv_floors)
+	go elevio.PollObstructionSwitch(drv_obstr)
+	go elevio.PollStopButton(drv_stop)
+
+	var state elev_state = INIT
+	for {
+		fmt.Printf("%v", state)
+		switch state {
+
+		case INIT:
+			//elevator reaches first floor, transistion to IDLE
+			elevio.SetMotorDirection(elevio.MD_Down)
+			select {
+			case a := <-drv_floors:
+				if a == numFloors-4 {					
+					state = IDLE
+				}
+			}
+		case IDLE:
+			//transitions to MOVE when an order is detected
+            //transisiton to EM_STOP
+            elevio.SetMotorDirection(elevio.MD_Stop)
+			select {
+			case a := <-drv_buttons:
+				targetFloor = a.Floor
+				state = MOVE
+			}
+
+		case MOVE:
+			//transisiton to wait when elevator reaches target floor
+			//transisiton to EM_stop
+			elevio.SetMotorDirection(elevio.MD_Up)
+			select {
+			case a := <-drv_floors:
+				if a == targetFloor {                    
+                    state = IDLE
+				}
+			}
+		case WAIT:
+		//transisiton to MOVE if there are pending orders
+		//transition to IDLE if not
+		case EM_STOP:
+		}
 	}
 }
--- main.go
+++ main.go
@@ -6,14 +6,23 @@ import (
 	"./elevio"
 )
 
+type elev_state int
+
+const (
+	INIT elev_state = iota
+	IDLE
+	MOVE
+	WAIT
+	EM_STOP
+)
+
 func main() {
+
+	var targetFloor int
 
 	numFloors := 4
 
 	elevio.Init("localhost:15657", numFloors)
-
-	var d elevio.MotorDirection = elevio.MD_Up
-	//elevio.SetMotorDirection(d)
 
 	drv_buttons := make(chan elevio.ButtonEvent)
 	drv_floors := make(chan int)
@@ -25,36 +34,146 @@ func main() {
 	go elevio.PollObstructionSwitch(drv_obstr)
 	go elevio.PollStopButton(drv_stop)
 
+	var state elev_state = INIT
 	for {
-		select {
-		case a := <-drv_buttons:
-			fmt.Printf("%+v\n", a)
-			elevio.SetButtonLamp(a.Button, a.Floor, true)
+		fmt.Printf("%v", state)
+		switch state {
 
-		case a := <-drv_floors:
-			fmt.Printf("%+v\n", a)
-			if a == numFloors-1 {
-				d = elevio.MD_Down
-			} else if a == 0 {
-				d = elevio.MD_Up
-			}
-			//elevio.SetMotorDirection(d)
-
-		case a := <-drv_obstr:
-			fmt.Printf("%+v\n", a)
-			if a {
-				elevio.SetMotorDirection(elevio.MD_Stop)
-			} else {
-				elevio.SetMotorDirection(d)
-			}
-
-		case a := <-drv_stop:
-			fmt.Printf("%+v\n", a)
-			for f := 0; f < numFloors; f++ {
-				for b := elevio.ButtonType(0); b < 3; b++ {
-					elevio.SetButtonLamp(b, f, false)
+		case INIT:
+			//elevator reaches first floor, transistion to IDLE
+			elevio.SetMotorDirection(elevio.MD_Down)
+			select {
+			case a := <-drv_floors:
+				if a == numFloors-4 {
+					state = IDLE
 				}
 			}
+		case IDLE:
+			//transitions to MOVE when an order is detected
+			//transisiton to EM_STOP
+			elevio.SetMotorDirection(elevio.MD_Stop)
+			select {
+			case a := <-drv_buttons:
+				targetFloor = a.Floor
+				state = MOVE
+			}
+
+		case MOVE:
+			//transisiton to wait when elevator reaches target floor
+			//transisiton to EM_stop
+			elevio.SetMotorDirection(elevio.MD_Up)
+			select {
+			case a := <-drv_floors:
+				if a == targetFloor {
+					state = IDLE
+				}
+			}
+		case WAIT:
+		//transisiton to MOVE if there are pending orders
+		//transition to IDLE if not
+		case EM_STOP:
 		}
 	}
+	/*
+							numFloors := 4
+
+							elevio.Init("localhost:15657", numFloors)
+
+							var d elevio.MotorDirection = elevio.MD_Up
+							//elevio.SetMotorDirection(d)
+
+							drv_buttons := make(chan elevio.ButtonEvent)
+							drv_floors := make(chan int)
+							drv_obstr := make(chan bool)
+							drv_stop := make(chan bool)
+
+							go elevio.PollButtons(drv_buttons)
+							go elevio.PollFloorSensor(drv_floors)
+							go elevio.PollObstructionSwitch(drv_obstr)
+							go elevio.PollStopButton(drv_stop)
+
+							for {
+								select {
+								case a := <-drv_buttons:
+									fmt.Printf("%+v\n", a)
+									elevio.SetButtonLamp(a.Button, a.Floor, true)
+
+								case a := <-drv_floors:
+									fmt.Printf("%+v\n", a)
+									if a == numFloors-1 {
+										d = elevio.MD_Down
+									} else if a == 0 {
+										d = elevio.MD_Up
+									}
+									//elevio.SetMotorDirection(d)
+
+								case a := <-drv_obstr:
+									fmt.Printf("%+v\n", a)
+									if a {
+										elevio.SetMotorDirection(elevio.MD_Stop)
+									} else {
+										elevio.SetMotorDirection(d)
+									}
+
+								case a := <-drv_stop:
+									fmt.Printf("%+v\n", a)
+									for f := 0; f < numFloors; f++ {
+										for b := elevio.ButtonType(0); b < 3; b++ {
+									var targetFloor int
+
+					numFloors := 4
+
+					elevio.Init("localhost:15657", numFloors)
+
+					drv_buttons := make(chan elevio.ButtonEvent)
+					drv_floors := make(chan int)
+					drv_obstr := make(chan bool)
+					drv_stop := make(chan bool)
+
+					go elevio.PollButtons(drv_buttons)
+					go elevio.PollFloorSensor(drv_floors)
+					go elevio.PollObstructionSwitch(drv_obstr)
+					go elevio.PollStopButton(drv_stop)
+
+					var state elev_state = INIT
+					for {
+						switch state {
+
+						case INIT:
+							//elevator reaches first floor, transistion to IDLE
+							elevio.SetMotorDirection(elevio.MD_Down)
+							select {
+							case a := <-drv_floors:
+								if a == numFloors-3 {
+									elevio.SetMotorDirection(elevio.MD_Stop)
+								}
+							}
+							state = IDLE
+						case IDLE:
+							//transitions to MOVE when an order is detected
+							//transisiton to EM_STOP
+							select {
+							case a := <-drv_buttons:
+								targetFloor = a.Floor
+								state = MOVE
+							}
+
+						case MOVE:
+							//transisiton to wait when elevator reaches target floor
+							//transisiton to EM_stop
+							elevio.SetMotorDirection(elevio.MD_Up)
+							select {
+							case a := <-drv_floors:
+								if a == targetFloor {
+									elevio.SetMotorDirection(elevio.MD_Stop)
+								}
+							}
+						case WAIT:
+						//transisiton to MOVE if there are pending orders
+						//transition to IDLE if not
+						case EM_STOP:
+						}
+		            }
+	*/
+
 }
