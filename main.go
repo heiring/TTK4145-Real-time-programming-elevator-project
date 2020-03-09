36e70104eafe08f6b686915f221473f16ea0412a
csletten <c.sletten99@gmail.com>
2020-03-09 17:55:45 +0100

    network test in main

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
-	"./fsm"
+	//"./fsm"
+	."./network"
+	"fmt"
+	"time"
 )
 
 func main() {
-	fsm.FSM(1)
+	//fsm.FSM(1)
+	elevatorSliceCh := make(chan [3]Elevator)
+	var counter = 0
+	go ElevatorLifeStatusMonitor(elevatorSliceCh)
+
+	for {
+		fmt.Printf("iteration ")
+		counter ++
+		fmt.Println(counter)		
+
+		select {
+		case p := <- elevatorSliceCh:
+			for _,elevator := range p{
+				
+				fmt.Printf("elevator with id: ")
+				fmt.Printf(elevator.Id)
+				fmt.Printf( "is alive? ")
+				fmt.Println(elevator.IsAlive)
+				
+			}
+			time.Sleep(1000 * time.Millisecond) 
+		}
+	}	
+	
+	
 }
--- network/network_module.go
+++ network/network_module.go
@@ -9,75 +9,77 @@ import (
 	"./utilities/peers"
 )
 
-type elevator struct {
-	id      string
-	isAlive bool
+type Elevator struct {
+	Id      string
+	IsAlive bool
 }
 
-var elevator0 elevator
-var elevator1 elevator
-var elevator2 elevator
+var elevator0 Elevator
+var elevator1 Elevator
+var elevator2 Elevator
 
-var elevatorSlice [3]elevator
+var elevatorSlice [3]Elevator
 
 func initializeElevatorStructs() {
-	elevatorSlice[0].isAlive = false
-	elevatorSlice[1].isAlive = false
-	elevatorSlice[2].isAlive = false
-	//id??
+	elevatorSlice[0].IsAlive = false
+	elevatorSlice[1].IsAlive = false
+	elevatorSlice[2].IsAlive = false
+	//Id??
 }
 
 // AliveTransmission transmits an AliveMsg every second
-func AliveTransmission(elevatorSliceCh chan<- [3]elevator) {
+func ElevatorLifeStatusMonitor(elevatorSliceCh chan<- [3]Elevator) {
 
 	initializeElevatorStructs()
 	initialUpdateComplete := false
 
 	//slice containing 3 elevator structs
-	elevatorSlice := [3]elevator{elevator0, elevator1, elevator2}
+	elevatorSlice := [3]Elevator{elevator0, elevator1, elevator2}
 
 	//initialize peerUpdate communication
 	localIP, _ := localip.LocalIP()
-	id := fmt.Sprintf("peer-%s-%d", localIP, os.Getpid())
+	Id := fmt.Sprintf("peer-%s-%d", localIP, os.Getpid())
 	peerUpdateCh := make(chan peers.PeerUpdate)
 	peerTxEnable := make(chan bool)
-	go peers.Transmitter(15647, id, peerTxEnable) //which port???
+	go peers.Transmitter(15647, Id, peerTxEnable) //which port???
 	go peers.Receiver(15647, peerUpdateCh)
 
 	for {
 		select {
 		case p := <-peerUpdateCh:
+			/*
 			fmt.Printf("Peer update:\n")
 			fmt.Printf("  Peers:    %q\n", p.Peers)
 			fmt.Printf("  New:      %q\n", p.New)
 			fmt.Printf("  Lost:     %q\n", p.Lost)
-
+			*/
 			if len(p.Peers) == 3 && initialUpdateComplete == false {
-				elevatorSlice[0].isAlive = true
-				elevatorSlice[0].id = p.Peers[0]
+				//assume peer Id always remain the same for every elecvator ??
+				elevatorSlice[0].IsAlive = true
+				elevatorSlice[0].Id = p.Peers[0]
 
-				elevatorSlice[1].isAlive = true
-				elevatorSlice[1].id = p.Peers[1]
+				elevatorSlice[1].IsAlive = true
+				elevatorSlice[1].Id = p.Peers[1]
 
-				elevatorSlice[2].isAlive = true
-				elevatorSlice[2].id = p.Peers[2]
+				elevatorSlice[2].IsAlive = true
+				elevatorSlice[2].Id = p.Peers[2]
 
 				initialUpdateComplete = true
 			}
 
 			if initialUpdateComplete {
-				//elevator is no longer alive if its ID is in p.Lost:
-				for _, id := range p.Lost {
+				//elevator is no longer alive if its Id is in p.Lost:
+				for _, Id := range p.Lost {
 					for _, elevator := range elevatorSlice {
-						if elevator.id == id {
-							elevator.isAlive = false
+						if elevator.Id == Id {
+							elevator.IsAlive = false
 						}
 					}
 				}
-				//if an elevator ID is in p.New, it's now alive
+				//if an elevator Id is in p.New, it's now alive
 				for _, elevator := range elevatorSlice {
-					if elevator.id == p.New {
-						elevator.isAlive = true
+					if elevator.Id == p.New {
+						elevator.IsAlive = true
 					}
 				}
 			}
@@ -85,9 +87,9 @@ func AliveTransmission(elevatorSliceCh chan<- [3]elevator) {
 		default:
 			// Do nothing
 
-		}
-		time.Sleep(1000 * time.Millisecond) //??
+		}		
 		elevatorSliceCh <- elevatorSlice
+		time.Sleep(1000 * time.Millisecond) //??
 
 	}
 }
