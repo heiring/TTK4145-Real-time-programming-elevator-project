a6d9873c566495671d23d5c0856343f1ab290ed8
Håvard Eiring <havard.eiring@gmail.com>
2020-04-01 14:40:14 +0200

    network testing

--- main.go
+++ main.go
@@ -3,29 +3,25 @@ package main
 import (
 	"fmt"
 
-	"./fsm"
-
 	"./network/network2"
 )
 
 func main() {
-
-	fsm.FSM(1)
 
 	//initialization for simulator
 	//numFloors := 3
 	//elevio.Init("localhost:15657", numFloors)
 
 	//network test
-	// elevatorStateTxCh := make(chan network2.ElevatorState)
-	// elevatorStateRxCh := make(chan network2.ElevatorState)
+	elevatorStateTxCh := make(chan network2.ElevatorState)
+	elevatorStateRxCh := make(chan network2.ElevatorState)
 
-	//transmitPacketCh := make(chan network2.ElevatorState)
+	transmitPacketCh := make(chan network2.ElevatorState)
 	//stateUpdateCh := make(chan network2.ElevatorState)
 
 	//lostIDCh := make(chan string)
 
-	//go network2.BroadcastElevatorState(transmitPacketCh, elevatorStateTxCh, 500)
+	go network2.BroadcastElevatorState(transmitPacketCh, elevatorStateTxCh, 500)
 	//go network2.ListenElevatorState(elevatorStateRxCh, stateUpdateCh, 10000, lostIDCh)
 
 	// go bcast.Transmitter(19569, elevatorStateTxCh)
@@ -36,7 +32,8 @@ func main() {
 	yeet := network2.ElevatorState{ID: "2222", IsAlive: true}
 
 	for {
-		elevatorStateTxCh <- yeet
+		//elevatorStateTxCh <- yeet
+		transmitPacketCh <- yeet
 		select {
 		case y := <-elevatorStateRxCh:
 			fmt.Println("packet received")
